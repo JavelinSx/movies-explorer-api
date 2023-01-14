17a233e8fbdd4f74fb2f9bcbcd269a42f92f2623
JavelinSx <74298153+JavelinSx@users.noreply.github.com>
2023-01-14 13:32:49 +0300

    add beatfilm to cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,12 +1,11 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
   'http://javelin.nomoredomains.icu',
   'https://javelin.nomoredomains.icu',
   'http://api.javelin.nomoredomains.icu',
   'https://api.javelin.nomoredomains.icu',
+  'https://api.nomoreparties.co/beatfilm-movies',
 ];
 
 module.exports = (req, res, next) => {
